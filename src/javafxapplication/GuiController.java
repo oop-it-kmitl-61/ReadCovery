f58df005709b2055b3b3c1afccbcc237e19c0a87
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-11-22 22:39:16 +0700

    add changeScene function

--- src/javafxapplication/GuiController.java
+++ src/javafxapplication/GuiController.java
@@ -4,10 +4,33 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import com.jfoenix.*;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class GuiController {
-    @FXML private JFXButton registBtn;
+    @FXML private JFXButton registBtn, backBtn;
     @FXML void registerHandler(ActionEvent event){
-//        registBtn.setText("wow");
+        registBtn.setText("wow");
     }
+    @FXML void changeScene(Stage stage, String newScene){
+        try {
+            Parent newRoot = FXMLLoader.load(getClass().getResource(newScene));
+            Scene login = new Scene(newRoot);
+            stage.setScene(login);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    @FXML void backHandler(ActionEvent e)  {
+        Stage stage = (Stage)backBtn.getScene().getWindow();
+        changeScene(stage, "loginjfoenix.fxml");
+    }
+
 }
