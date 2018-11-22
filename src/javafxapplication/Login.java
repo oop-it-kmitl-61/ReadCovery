ec560e5cacca5fd62de2f0c26303f271d2f96254
TheNlesh <32976194+TheNlesh@users.noreply.github.com>
2018-11-22 17:39:25 +0700

    Add files via upload

---
+++ src/javafxapplication/Login.java
@@ -0,0 +1,25 @@
+package javafxapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
