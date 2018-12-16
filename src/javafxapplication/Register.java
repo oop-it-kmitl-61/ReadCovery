6d7ae44489c50220c223fc7cee7716b531c79e29
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-12-16 18:20:35 +0700

    Auto stash before merge of "GUI" and "origin/GUI"

--- src/javafxapplication/Register.java
+++ src/javafxapplication/Register.java
@@ -2,11 +2,15 @@ package javafxapplication;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Register extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Register extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Register.fxml"));
