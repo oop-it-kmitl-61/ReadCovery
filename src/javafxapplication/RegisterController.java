f659db08d79ee80b0c3193e5f3b3e14bf3217853
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-12-17 14:59:45 +0700

    Add RegisterController

--- src/javafxapplication/RegisterController.java
+++ src/javafxapplication/RegisterController.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class RegisterController implements Initializable {
     @FXML private JFXTextField regEmailTf,regUserTf;
     @FXML private JFXPasswordField regPassPf,regCpTf;
     @FXML private Label regErrLb;
+    private String errMsg;
 
 
     @FXML void changeScene(Stage stage, String newScene){
@@ -36,16 +38,39 @@ public class RegisterController implements Initializable {
         changeScene(stage, "Loginjfoenix.fxml");
     }
     @FXML void registerHandler(ActionEvent event){
-        System.out.println("Email:" +regEmailTf.getText());
-        System.out.println("User:"+regUserTf.getText());
-        System.out.println("Pass:"+regPassPf.getText());
-        System.out.println("Confirm-Pass:"+regCpTf.getText());
+//        System.out.println("Email:" +regEmailTf.getText());
+//        System.out.println("User:"+regUserTf.getText());
+//        System.out.println("Pass:"+regPassPf.getText());
+//        System.out.println("Confirm-Pass:"+regCpTf.getText());
         if(regEmailTf.getText().equals("")){
-            regErrLb.setText("Error!");
+            errMsg += "Email is empty\n";
         }
+        if(regUserTf.getText().equals("")){
+            errMsg += "Username is empty\n";
+        }
+        if(regPassPf.getText().equals("") || regCpTf.getText().equals("")){
+            errMsg += "Password or Confirm-Password is empty\n";
+        }
+        //Password and confirm Password is not equal
+        if(!regPassPf.getText().equals(regCpTf.getText())){
+            errMsg += "Password and Confirm-Password isn't match";
+        }
+        //if not err
+        if(errMsg.equals("")){
+            regErrLb.setTextFill(Color.GREEN);
+            regErrLb.setText("Register Success!");
+            errMsg="";
+        }else{
+            regErrLb.setTextFill(Color.RED);
+            regErrLb.setText(errMsg);
+            errMsg ="";
+        }
+
     }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //set default value of errMsg don't be null
+        errMsg="";
 
     }
 }
