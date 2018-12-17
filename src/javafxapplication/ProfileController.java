9e8c2b5d748ab6f7e274bf0a8a49534f4e3f079e
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-12-17 14:45:36 +0700

    delete unuse line in ProfileController

--- src/javafxapplication/ProfileController.java
+++ src/javafxapplication/ProfileController.java
@@ -11,7 +11,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
