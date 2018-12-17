f5dca3214973ec5e90bf78397673d5426857eb2c
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-12-17 14:22:29 +0700

    Clean waste code in RegController

--- src/javafxapplication/RegisterController.java
+++ src/javafxapplication/RegisterController.java
@@ -11,7 +11,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
