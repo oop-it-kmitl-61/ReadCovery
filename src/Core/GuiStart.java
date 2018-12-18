443ceb5c732d8652a027f6c7aa9463df2c12d480
Puttimate "Jusmistic" Thammasang <jusmistic@gmail.com>
2018-12-18 22:17:56 +0700

    Clean guiStart

--- src/Core/GuiStart.java
+++ src/Core/GuiStart.java
@@ -1,37 +1,21 @@
 package Core;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
+
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
+
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-import org.json.JSONArray;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.ResourceBundle;
+import javafx.stage.Stage;
+
 
 public class GuiStart extends Application{
-    private static final String API ="3249e937bdde4f27bc283ab7219b1142";
-    private static ApiUtil api;
-    private static JSONArray articles;
-    private static JSONArray data_pool;
 
-    public AppAction app;
-
-    @FXML
-    private ImageView content;
 
     @Override
     public void start(Stage stage) throws Exception {
 
-//        app.setSelects(apiData);
-//        app.prepInstance();
         Parent root = FXMLLoader.load(getClass().getResource("/javafxapplication/Loginjfoenix.fxml"));
         
         Scene scene = new Scene(root);
